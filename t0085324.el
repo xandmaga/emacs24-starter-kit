7739894db194c61d1fbfa7c862c852c19f4d0a9e
t0085324 <alexandre.furtado@tjmg.jus.br>
2014-10-16 18:27:58 -0300

    adicioando extensao para exportar para mediawiki

--- t0085324.el
+++ t0085324.el
@@ -1,5 +1,3 @@
-;;; comentando o org
-;;; (require 'ox-twiki)
-;;; (add-to-list 'load-path "~/.emacs.d/pacotes/mediawiki")"
-;;; (require 'ox-mediawiki)
+(add-to-list 'load-path "~/.emacs.d/pacotes/mediawiki")
+(require 'ox-mediawiki)
 (server-start)
