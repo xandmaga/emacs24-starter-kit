0cc7ec912782ac988e26cea71c1db0a4a85e8407
t0085324 <alexandre.furtado@tjmg.jus.br>
2014-12-02 17:30:44 -0200

    modificando customizacoes

--- t0085324.el
+++ t0085324.el
@@ -1,3 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (add-to-list 'load-path "~/.emacs.d/pacotes/mediawiki")
 (require 'ox-mediawiki)
 (server-start)
